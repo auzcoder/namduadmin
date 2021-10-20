92ed0dfbbdec49a406dbe53cb55741e8a9a17acc
namduadmin <abdulkhafizuz@gmail.com>
2021-10-20 10:20:36 +0500

    first commit

---
+++ JS/script.js
@@ -0,0 +1,35 @@
+"use strict";
+// 1
+
+// const savol = [];
+// savol[0] = prompt("Ismingiz?");
+// savol[1] = prompt("Familyangiz?");
+// savol[2] = +prompt("Yoshingiz?");
+
+// document.write("<hr>Ismi -  ", [savol[0]] + "<br><hr>");
+// document.write("Familyasi - ", [savol[1]] + "<br><hr>");
+// document.write("Yoshi - ", [savol[2]] + "<br><hr>");
+
+// 2
+// let a;
+// const son = ["Abdulhafiz", "Davlatov"];
+// alert(son[0] + son[1]);
+
+// Aylana uzunligi!
+// l=2pir*r
+// let l, r;
+// const pi = 3.14;
+// r = +prompt("Radiusni kiriting?", "");
+// l = 2 * pi * r * r;
+// document.write("Uzunligi = " + l);
+// alert(l);
+
+const savol = [];
+
+savol[0] = prompt("Ismingizni kiriting?", "");
+savol[1] = prompt("Familyangizni kiriting?", "");
+savol[2] = +prompt("Yoshingiz?", "");
+
+document.write("Ismi - " + savol[0] + "<br>");
+document.write("Familyasi - " + savol[1] + "<br>");
+document.write("Yoshi - " + savol[2] + "<br>");
